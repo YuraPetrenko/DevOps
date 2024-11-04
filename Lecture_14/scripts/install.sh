92bd8b45ef5a1fab42a38161e37b1d8964bc0baa
Yura Petrenko <bald2003@ukr.net>
2024-11-04 17:39:05 +0200

    Lecture_17

--- Lecture_14/scripts/install.sh
+++ Lecture_14/scripts/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-sudo su
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-apt update
-apt install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
+sudo apt-get update
+    sudo apt-get install -y gnupg curl
+
+    curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg --dearmor -o /usr/share/keyrings/mongodb-server-8.0.gpg
+    echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
+    sudo systemctl enable mongod
+    sudo systemctl start mongod
---
+++ Lecture_17/Readme.md
@@ -0,0 +1,12 @@
+docker-compose ps
+![Результат роботи скрипта]( Screenshots/docker-compose ps.PNG)
+
+docker network ls
+![Результат роботи скрипта]( Screenshots/docker network ls.PNG)
+
+
+docker volume ls
+![Результат роботи скрипта]( Screenshots/docker volume ls.PNG)
+
+docker exec -it <ad9749203643b4bb2ed7c1d8a1147c4408b9a24fdfe35450b1d30ab1e2f91ff7> psql -U postgres
+![Результат роботи скрипта]( Screenshots/docker exec.PNG)
